33312102e272842d26fd433da777b1f4544c8a86
cesarmanzocode <cesarmanzocode@gmail.com>
2026-03-28 13:42:33 -0600

    routes/__init__.py updated

--- app/presentation/routes/__init__.py
+++ app/presentation/routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from app.presentation.routes.health import router as health_router
 from app.presentation.routes.pages import router as pages_router
 
 
 router = APIRouter()
 router.include_router(pages_router)
+router.include_router(health_router)
